ab2ef8b5fdf795d77d5e3aa4eb6b91029404566c
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-09 19:15:24 +0530

    daily com 11

--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -1,1 +1,12 @@
-fdfs
+var nameVar = 'Andrew';
+var nameVar = 'Mike';
+
+console.log('nameVar', nameVar);
+
+let naemLet = 'Jen';
+let nameLet = 'Julie'
+console.log('nameLet', nameLet);
+
+const nameConst = 'alpha';
+const nameConst = 'jayawardane';
+console.log('nameConst', nameConst);
