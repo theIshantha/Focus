2cd2181e6e2e5429198213b126666b0f3c3c16b6
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-09 19:39:24 +0530

    daily com 13

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,5 +10,12 @@ nameLet = 'Julie';
 console.log('nameLet', nameLet);
 
 var nameConst = 'alpha';
-
 console.log('nameConst', nameConst);
+
+var fullName = 'Andrew Mead';
+
+if (fullname) {
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+console.log(firstName);
--- src/playground/es6-let-const.js
+++ src/playground/es6-let-const.js
@@ -8,5 +8,13 @@ nameLet = 'Julie'
 console.log('nameLet', nameLet);
 
 const nameConst = 'alpha';
-
 console.log('nameConst', nameConst);
+
+
+var fullName = 'Andrew Mead';
+
+if(fullname){
+    var firstName = fullName.split(' ')[0];
+    console.log(firstName);
+}
+console.log(firstName);
