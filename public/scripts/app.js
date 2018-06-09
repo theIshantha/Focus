0fccdfcabc83936c1c2269314254b4b9648a26bb
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-09 11:17:30 +0530

    daily com 1

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,9 +15,37 @@ var template = React.createElement(
     React.createElement(
         'p',
         null,
-        'This is some info'
+        'shit always happens'
     )
 );
+
+var template1 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        ' Ishantha Udara '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' About Me :- Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry \'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Age :- 22 '
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' Location :- Colombo '
+    )
+);
+
 var appRoot = document.getElementById('app');
+var appRoot1 = document.getElementById('app1');
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(template1, appRoot1);
--- src/app.js
+++ src/app.js
@@ -4,10 +4,24 @@ console.log('App.js is running');
 
 var template = ( 
     <div>
-    <h1>Focus App</h1>
-    <p>This is some info</p>
+        <h1>Focus App</h1>
+        <p>shit always happens</p>
     </div>
 );
-var appRoot = document.getElementById('app');
+
+var template1 = (
+    <div>
+    <h1> Ishantha Udara </h1>
+    <p> About Me :- Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+    <p> Age :- 22 </p>
+    <p> Location :- Colombo </p>
+
+    </div>
+);
+
+
+var appRoot  = document.getElementById('app');
+var appRoot1 = document.getElementById('app1');
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(template1, appRoot1);
