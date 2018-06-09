4183cfd565480bc7ce3fa7a8d2e2249b3f54a58d
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-09 18:34:08 +0530

    daily com 8

--- src/app.js
+++ src/app.js
@@ -1,51 +1,39 @@
 console.log('App.js is running');
 
-var template = {
-    title: 'React test app',
-    subtitle: 'focus app',
-}
+// var template = {
+//     title: 'React test app',
+//     subtitle: 'focus app',
+// }
 
 
 // JSX - JavaScript XML
 
-var template = ( 
-    <div>
-        <h1>{template.title}</h1>
-        <p>{template.subtitle}</p>
-    </div>
-);
+var app ={
+    title: 'Focus App',
+    subtitle: 'The TODO App',
+    option: ['One', 'Two'] 
 
-var user = {
-    // name: 'Andrew',
-    age : '26',
-    location : 'Philadelphia'
-};
- 
-function getLocation(location) {
-    if(location){
-        return location;
-    }else{
-        return 'Unknown';
-    }
 }
 
-var template1 = (
+var template = (
     <div>
-    <h1> { user.name ? user.name : 'Anonymous' } </h1>
-    <p> About Me :- Recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    <p> Age :- 22 </p>
-    <p> Location :- {getLocation(user.location)} </p>
+        <h1>{app.title}</h1>
+        app.subtitle && <p1>{app.subtitle}</p1>
 
-    </div>
+        <ol> 
+            <li></li>
+            <li></li>
+        </ol>
+
+    <div>
 );
+
 
 
 
 var appRoot  = document.getElementById('app');
-var appRoot1 = document.getElementById('app1');
 
 ReactDOM.render(template, appRoot);
-ReactDOM.render(template1, appRoot1);
 
 
 
