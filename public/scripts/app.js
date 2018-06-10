3174f4b0d87910f3d0d3f54673ed131f4060154d
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-10 09:16:08 +0530

    daily com 21

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,27 @@
-"use strict";
+'use strict';
+
+// argument object 
+
+var add = function add(a, b) {
+
+    console.log(arguments);
+    return a + b;
+};
+
+console.log(add(55, 1));
+
+//this keyword - no longer bound 
+
+var user = {
+    name: 'Andrew',
+    cities: ['Philadelphia', 'New York', 'Dublin'],
+    printPlacesLived: function printPlacesLived() {
+        console.log(this.name);
+        console.log(this.cities);
+
+        this.cities.forEach(function (city) {
+            console.log(this.name + ' has lived in ' + city);
+        });
+    }
+};
+user.printPlacesLived();
---
+++ src/playground/es6-arrow-function-2.js
@@ -0,0 +1,26 @@
+// argument object 
+
+const add = function (a, b) {
+
+    console.log(arguments);
+    return a + b ;
+
+}
+
+console.log(add(55, 1));
+
+//this keyword - no longer bound 
+
+const user = {
+    name: 'Andrew',
+    cities: ['Philadelphia','New York','Dublin'],
+    printPlacesLived: function () {
+        console.log(this.name);
+        console.log(this.cities);
+
+        this.cities.forEach(function(city){
+            console.log(this.name + ' has lived in ' + city );
+        });
+    }
+};
+user.printPlacesLived();
