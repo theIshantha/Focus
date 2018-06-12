bf2379ee4582872dd1af2fe7f3fc9b51ca91c870
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-12 22:58:38 +0530

    da

--- src/app.js
+++ src/app.js
@@ -18,14 +18,7 @@ var template = (
         <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
         
         <ol> 
-            <li>Item one
-loerem afnnfrnainraadasfaefaffefsfgrggghgsdgLorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry 's standard dummy text ever since the 1500s, when an unknown printer took a galle' +
-    'y of type and scrambled it to make a type specimen book. It has survived not onl' +
-        'y five centuries, but also the leap into electronic typesetting, remaining essen' +
-        'tially unchanged. It was popularised in the 1960s with the release of Letraset s' +
-        'heets containing Lorem Ipsum passages, and more recently with desktop publishing' +
-        ' software like Aldus PageMaker including versions of Lorem Ipsum.'
-            </li>
+            <li>Item one</li>
             <li>Item two</li>
         </ol>
 
