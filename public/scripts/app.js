2ec1983c5e0fa863cf5c488b4536f91adb28854f
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-12 22:44:36 +0530

    d

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -35,7 +35,7 @@ var template = React.createElement(
         React.createElement(
             'li',
             null,
-            'Item one abc'
+            'Item one abcd'
         ),
         React.createElement(
             'li',
