94b85ae7084ef1d2dafa081b25bdc0e39c1dad8b
Ishantha Udara <ishanthaudara96@gmail.com>
2018-06-19 22:18:10 +0530

    da

--- src/app.js
+++ src/app.js
@@ -1,58 +1,58 @@
 // JSX - JavaScript XML
 
-// var app ={
-//     title: 'Focus App',
-//     subtitle: 'The TODO App',
-//     options: ['One', 'Two'] 
-// }
+var app ={
+    title: 'Focus App',
+    subtitle: 'The TODO App',
+    options: ['One', 'Two'] 
+}
 
-// var template = (
-//     <div>
-//         <h1>{app.title}</h1>
-//         {app.subtitle && <p>{app.subtitle}</p>}
-//         <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
-//         <p></p>
-//         <ol> 
-//             <li>Item one</li>
-//             <li>Item two</li>
-//         </ol>
+var template = (
+    <div>
+        <h1>{app.title}</h1>
+        {app.subtitle && <p>{app.subtitle}</p>}
+        <p>{app.options.length > 0 ? 'Here are your options' : 'No options' }</p>
+        <p></p>
+        <ol> 
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
 
-//     </div>
-// );
+    </div>
+);
 
-// let count = 0;
+let count = 0;
 
-// const addOne = () => {
-//     count++;
-//     renderCounterApp();
-// };
-// const minusOne = () => {
-//     count--;
-//     renderCounterApp();
-// };
-// const reset = () => {
-//     count = 0;
-//     renderCounterApp();
-// };
+const addOne = () => {
+    count++;
+    renderCounterApp();
+};
+const minusOne = () => {
+    count--;
+    renderCounterApp();
+};
+const reset = () => {
+    count = 0;
+    renderCounterApp();
+};
 
-// var appRoot  = document.getElementById('app');
+var appRoot  = document.getElementById('app');
 
-// const renderCounterApp = () => {
-//     const templateTwo = (
-//         <div className="container">
-//             <br></br>
-//             <br></br>
-//             <br></br>
-//                 <h1>Count : {count}</h1>
-//                 <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={addOne} >&nbsp;&nbsp;&nbsp;&nbsp; +1 &nbsp;&nbsp;&nbsp;&nbsp;</button> &nbsp;&nbsp;
-//                 <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={minusOne} >&nbsp;&nbsp;&nbsp;&nbsp; -1 &nbsp;&nbsp;&nbsp;&nbsp;</button>  <br></br><br></br>
-//                 <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={reset} >&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Reset
-//                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</button>
-//         </div>
-//     );
+const renderCounterApp = () => {
+    const templateTwo = (
+        <div className="container">
+            <br></br>
+            <br></br>
+            <br></br>
+                <h1>Count : {count}</h1>
+                <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={addOne} >&nbsp;&nbsp;&nbsp;&nbsp; +1 &nbsp;&nbsp;&nbsp;&nbsp;</button> &nbsp;&nbsp;
+                <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={minusOne} >&nbsp;&nbsp;&nbsp;&nbsp; -1 &nbsp;&nbsp;&nbsp;&nbsp;</button>  <br></br><br></br>
+                <button id="my-id" className="button btn btn-outline-danger btn-sm" onClick={reset} >&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Reset
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</button>
+        </div>
+    );
 
-//     ReactDOM.render(templateTwo, appRoot);
+    ReactDOM.render(templateTwo, appRoot);
 
-// };
-//   renderCounterApp();
+};
+  renderCounterApp();
 
